7138cee992f2e0ff0be4375dcd954ff07c7f3d4d
İBRAHİM ELZAMİL <ibooalzam@gmail.com>
2022-02-01 00:09:22 +0300

    Add project files.

---
+++ src/rentACar/Domain/Entities/Car.cs
@@ -0,0 +1,35 @@
+﻿using CorePersistence.Reposittories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car : Entity
+    {
+      
+
+        public Car()
+        {
+
+        }
+
+        public Car(int id , int colorId, int modelId, string plate, short modelYear)
+        {
+            Id = id;
+            ColorId = colorId;
+            ModelId = modelId;
+            Plate = plate;
+            ModelYear = modelYear;
+        }
+        public int ColorId { get; set; }
+        public int ModelId { get; set; }
+        public string Plate { get; set; }
+        public short ModelYear { get; set; }
+        public virtual Color Color { get; set; }
+        public virtual Model Model { get; set; }
+
+    }
+}
